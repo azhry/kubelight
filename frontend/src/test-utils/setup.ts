7510346ad7a2d15ae70c8a58956961b3538751cc
Azhary Arliansyah <arliansyah_azhary@yahoo.com>
2026-06-21 19:35:29 +0700

    test(fe-013): add unit tests for context selector and namespace filter

--- frontend/src/test-utils/setup.ts
+++ frontend/src/test-utils/setup.ts
@@ -1,7 +1,14 @@
 import { cleanup } from "@testing-library/react";
-import { afterEach, vi } from "vitest";
+import { afterEach, vi, beforeAll } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import { mockInvoke, mockListen, mockOpen, resetTauriMocks } from "./tauri-mocks";
+
+beforeAll(() => {
+  window.HTMLElement.prototype.setPointerCapture = window.HTMLElement.prototype.setPointerCapture || (() => {});
+  window.HTMLElement.prototype.releasePointerCapture = window.HTMLElement.prototype.releasePointerCapture || (() => {});
+  window.HTMLElement.prototype.hasPointerCapture = window.HTMLElement.prototype.hasPointerCapture || (() => false);
+  window.HTMLElement.prototype.scrollIntoView = window.HTMLElement.prototype.scrollIntoView || (() => {});
+});
 
 afterEach(() => {
   cleanup();
